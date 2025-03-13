6e8d8e5a5baf22b563f9ff08e349b4dddf4ac15d
jorgepinedadacodes <166452815+jorgepinedadacodes@users.noreply.github.com>
2025-03-13 15:11:33 -0500

    feat: add environments and create models

---
+++ src/app/shared/models/task-response.model.ts
@@ -0,0 +1,6 @@
+export interface TaskResponse{
+    Id:number;
+    Title: string;
+    Description?: string;
+    IsCompleted: boolean;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    API: "https://taskmanagerapi20250313114734-hrehhsgzfzezhwdv.eastus-01.azurewebsites.net/"
+};
